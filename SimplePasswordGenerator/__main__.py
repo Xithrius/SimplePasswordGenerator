eb4e5437e4ae5373d9281e7c1f0e223547aa2976
Bast <bast@fastmail.com>
2021-05-13 05:05:50 -0700

    Switch from tk to ttk widgets

--- SimplePasswordGenerator/__main__.py
+++ SimplePasswordGenerator/__main__.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from secrets import token_urlsafe
 import pyperclip
 
@@ -16,23 +17,25 @@ class MainWindow(tk.Tk):
         self.char_amount.set(22)
 
         # First section
-        self.l1 = tk.Label(self, text='Password length:')
+        self.l1 = ttk.Label(self, text='Password length:')
         self.l1.grid()
 
-        self.e1 = tk.Entry(self, textvariable=self.char_amount)
+        self.e1 = ttk.Entry(self, textvariable=self.char_amount)
         self.e1.grid(row=0, column=1)
 
-        self.b1 = tk.Button(self,
+        self.b1 = ttk.Button(self,
                             text='Generate password.',
                             command=lambda: self.generate_password())
         self.b1.grid(row=0, column=2)
 
         # Second section:
-        self.l2 = tk.Label(self, text='No password generated.', fg='red')
         self.l2.grid(row=1)
+        self.l2_style = ttk.Style()
+        self.l2_style.configure("L2.TLabel", foreground='red')
+        self.l2 = ttk.Label(self, text='No password generated.', style='L2.TLabel')
 
         # Third section:
-        self.b2 = tk.Button(self,
+        self.b2 = ttk.Button(self,
                             text='Copy to clipboard.',
                             command=lambda: self.copy_to_clipboard(),
                             state='disabled')
@@ -42,8 +45,9 @@ class MainWindow(tk.Tk):
         self.b2['text'] = 'Copy to clipboard.'
         self.b2['state'] = 'normal'
 
-        self.l2['fg'] = 'green'
+        self.l2_style.configure('L2.TLabel', foreground='green')
         self.l2['text'] = 'Password generated!'
+        self.l2['style'] = "L2.TLabel"
 
         self.password = token_urlsafe(self.char_amount.get())
 
